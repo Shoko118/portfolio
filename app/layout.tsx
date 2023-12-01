881522ae75fccdb0832e4ccccd7961715b80e6fc
Bryan Liaw <bryan.liaw@involve.asia>
2023-12-01 14:20:22 +0800

    :bug: Fix deployment vercel issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,29 @@
-import Header from "@/components/Header";
-import "./globals.css";
-import type { Metadata } from "next";
-import { Gabarito } from "next/font/google";
-import { cn } from "@/lib/utils";
+import Header from '@/components/Header';
+import './globals.css';
+import type { Metadata } from 'next';
+import { Gabarito } from 'next/font/google';
+import { cn } from '@/lib/utils';
 
 const gabarito = Gabarito({
-  subsets: ["latin"],
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
   title: {
-    default: "Bryan Portfolio",
-    template: "%s | Bryan Portfolio",
+    default: 'Bryan Portfolio',
+    template: '%s | Bryan Portfolio',
   },
   openGraph: {
     title: `Bryan Portfolio`,
-    description: "Bleeding Edge Techs with NEXT App",
+    description: 'Bleeding Edge Techs with NEXT App',
     siteName: `Bryan Portfolio`,
-    url: "https://bryan-portfolio.vercel.app",
+    url: 'https://bryan-portfolio.vercel.app',
   },
-  description: "Bleeding Edge with NEXT APP",
-  metadataBase: new URL("https://bryan-portfolio.vercel.app"),
+  description: 'Bleeding Edge with NEXT APP',
+  metadataBase: new URL('https://bryan-portfolio.vercel.app'),
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={cn(gabarito.className)}>
