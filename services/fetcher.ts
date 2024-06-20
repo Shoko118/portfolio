3009e911751488dbd00d7ad3b811fc1d45cc97c2
Bryan Liaw <bryanliaw@Bryans-MacBook-Pro.local>
2024-06-20 14:34:01 +0800

    added zod t3 env and zustand package and create a new folder for zustand page

--- services/fetcher.ts
+++ services/fetcher.ts
@@ -1,7 +1,8 @@
+import { env } from '@/lib/env';
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://fakestoreapi.com',
+  baseURL: env.NEXT_PUBLIC_BASE_URL,
 });
 
 const fetcher = (url: string) => axiosInstance.get(url).then(res => res.data);
