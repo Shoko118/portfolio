62410d374cd7b70e03b92f63c4b412c90c19e210
Shoko <secast118@gmail.com>
2024-07-05 19:07:51 +0800

    config prettier tailwind config and global css and added custom ui and modify auth feature

--- app/project/form/page.tsx
+++ app/project/form/page.tsx
@@ -1,7 +1,20 @@
+import { Card, CardContent, CardFooter, CardHeader } from '@/packages/card';
+
 export default function FormPage() {
   return (
     <section>
-      <div>Welcome to form page</div>
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardHeader.Title>Card Title</CardHeader.Title>
+          <CardHeader.Description>Card Description</CardHeader.Description>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </section>
   );
 }
